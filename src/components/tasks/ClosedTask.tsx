d5692994e32f1dea9a7b2fa92883e5d450ca4998
Surya S <suryah2806@gmail.com>
2025-03-23 18:25:28 +0530

    Refactor ClosedTask component to simplify deadline handling and improve styles; update button alert color in global styles

--- src/components/tasks/ClosedTask.tsx
+++ src/components/tasks/ClosedTask.tsx
@@ -17,18 +17,18 @@ interface ClosedTask {
 }
 
 export default function ClosedTask({ _id, _title, _description, _deadline, _completed, _color_code, updateComplete, onDelete }: ClosedTask) {
-    const [deadline, setDeadline] = useState<Date | null>(_deadline ? new Date(_deadline) : null)
 
-    const passedDeadline = deadline ? new Date(deadline).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0) : false
+    const passedDeadline = _deadline ? new Date(_deadline).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0) : false
 
     return (
-        <div className={`flex flex-col rounded-md p-2 w-48 h-44 gap-2 border border-gray-300`} style={{ backgroundColor: _color_code }}>
+        <div className={`flex flex-col rounded-md p-2 w-48 h-44 gap-1 border border-gray-300`} style={{ backgroundColor: _color_code }}>
             <div className='flex items-center justify-between gap-2'>
                 <span className='text-md p-1 w-full border-0 outline-0 bg-transparent text-ellipsis overflow-hidden whitespace-nowrap'>
                     {_title}
                 </span>
-                <div className="shrink-0">
+                <div>
                     <Switch
+                        className='z-0'
                         onChange={(val) => updateComplete(_id, val)}
                         checked={_completed}
                         width={36}
@@ -37,7 +37,7 @@ export default function ClosedTask({ _id, _title, _description, _deadline, _comp
                 </div>
             </div>
 
-            <p className='text-xs p-1 border-0 outline-0 h-24 resize-none overflow-hidden bg-transparent line-clamp-3'>
+            <p className='text-sm text-gray-600 p-1 border-0 outline-0 h-24 resize-none overflow-hidden line-clamp-3'>
                 {_description}
             </p>
 
@@ -45,18 +45,8 @@ export default function ClosedTask({ _id, _title, _description, _deadline, _comp
                 <button className={`custom-button custom-button-small ${passedDeadline ? 'custom-button-alert' : ''}`}>
                     <div className='flex items-center gap-2 text-xs'>
                         <FaRegCalendarAlt size={12} />
-                        {deadline && deadline.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}
+                        {_deadline && new Date(_deadline).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}
                     </div>
-                    {deadline &&
-                    <span
-                        className="cursor-pointer"
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            setDeadline(null)
-                        }}
-                    >
-                        <IoMdCloseCircle size={16} />
-                    </span>}
                 </button>
                 <button
                     onClick={() => onDelete(_id)}
