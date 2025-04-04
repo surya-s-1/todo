c8c9491cbc9f553299ee1f02b22b5bebc08b4243
Surya S <suryah2806@gmail.com>
2025-04-04 15:56:14 +0530

    added output export for static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export',
   basePath: '/todo',
   reactStrictMode: true,
   devIndicators: false
