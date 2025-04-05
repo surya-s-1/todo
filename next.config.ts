cff10726c66c2750adcadde14fd7a936d331c191
Surya S <suryah2806@gmail.com>
2025-04-05 11:23:01 +0530

    added static export options in next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/todo',
+  // assetPrefix: '/todo',
+  // trailingSlash: true,
+  // skipTrailingSlashRedirect: true,
+  // output: 'export',
   reactStrictMode: true,
   devIndicators: false
 };
