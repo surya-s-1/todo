d5920e0e205cf7586eb2e2c2e640e685818ab12b
Surya <suryah2806@gmail.com>
2026-04-12 20:38:47 +0530

    Update basePath to use environment variable

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? `/todo` : '',
+  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   images: {
     unoptimized: true,
   },
